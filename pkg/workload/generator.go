cdea3e90b643a00169d36c62915e87ae381dd6bf
Abdul Dakkak <abduld@wolfram.com>
2018-03-07 07:26:33 -0500

    work

--- pkg/workload/generator.go
+++ pkg/workload/generator.go
@@ -7,6 +7,11 @@ import (
 	"gonum.org/v1/gonum/stat/samplemv"
 )
 
+// Pareto
+// Zipf
+// Uniform
+// best case : only once choice
+// Worst case : always evict
 var Generator rand.Rand
 var Distributions0 samplemv.Sampler
 var Distributions []distuv.Rander
