80cc4a04feb0f27391f25b0b6b7d21356fed7afb
Cheng Li <cli99@illinois.edu>
2018-03-14 14:53:32 -0500

    udpate print record

--- pkg/gpuinfo/gpuinfo_linux.go
+++ pkg/gpuinfo/gpuinfo_linux.go
@@ -151,11 +151,17 @@ func (m *System) Write(fmt string, output io.Writer) error {
 func (m *System) dsvHeader() []string {
 	return []string{
 		"device_idx",
-		"time_stamp",
+    "time_stamp",
+    "power",
+    "temperature",
+    "utilization"
 		"memory_used",
 		"human_memory_used",
-		"memory_free",
-		"human_memory_free",
+    "clock_core",
+    "clock_memory",
+    "pci_throughput_rx",
+    "pci_throughput_tx",
+    "num_processes",
 	}
 }
 
@@ -166,78 +172,138 @@ func (m *System) dsvRows() [][]string {
 		"---",
 		"---",
 		"---",
+    "---",
+    "---",
+		"---",
+		"---",
+		"---",
+		"---",
 		"---",
 	}
 	fullOutput := m.fullOutput
-	rows := [][]string{}
-	totalUsed := []uint64{}
-	totalFree := []uint64{}
+  rows := [][]string{}
+  totalPower :=  []uint64{}
+  totalTemperature :=  []uint64{}
+  totalUtilization := []uint64{}
+  totalMemoryUsed := []uint64{}
+  totalClockCore :=  []uint64{}
+  totalClockMemory :=  []uint64{}
+  totalPCIThroughputRX := []uint64{}
+	totalPCIThroughputTX := []uint64{}
+	totalNumProcesses := []uint64{}
 	totalEntries := []uint64{}
 	for _, dev := range m.devices {
-		currTotalUsed := uint64(0)
-		currTotalFree := uint64(0)
-		devIdx := cast.ToString(dev.index)
+    currTotalPower :=  uint64(0)
+    currTotalTemperature :=  uint64(0)
+    currTotalUtilization := uint64(0)
+    currTotalMemoryUsed := uint64(0)
+    currTotalClockCore := uint64(0)
+    currTotalClockMemory := uint64(0)
+    currTotalPCIThroughputRX := uint64(0)
+    currTotalPCIThroughputTX := uint64(0)
+    currTotalNumProcesses := uint64(0)
+
+    devIdx := cast.ToString(dev.index)
 		for _, entry := range dev.entries {
-			mem := entry.Memory
-			memoryUsed := mem.Used
-			memoryFree := mem.Free
-			currTotalUsed += memoryUsed
-			currTotalFree += memoryFree
+      power = entry.Power
+      temperature = entry.Temperature
+      utilization = entry.UtilizationInfo.GPU
+      memoryUsed = entry.nvmlMemoryInfo.GlobalUsed
+      clockCore = entry.ClockInfo.Core
+      clockMemory = entry.ClockInfo.Memory
+      PCIThroughputRX += entry.PCIStatusInfo.Throughput.RX
+      PCIThroughputTX += entry.PCIStatusInfo.Throughput.TX
+      numProcesses += len(entry.Processes)
+
+      currTotalPower += power
+      currTotalTemperature += temperature
+      currTotalUtilization += utilization
+      currTotalMemoryUsed += memoryUsed
+      currTotalClockCore += clockCore
+      currTotalClockMemory += clockMemory
+      currTotalPCIThroughputRX += PCIThroughputRX
+      currTotalPCIThroughputTX += PCIThroughputTX
+      currTotalNumProcesses += numProcesses
+
 			if fullOutput {
 				rows = append(
 					rows,
 					[]string{
 						devIdx,
-						entry.TimeStamp.Format(time.RFC3339Nano),
+            entry.TimeStamp.Format(time.RFC3339Nano),
+            cast.ToString(power)
+            cast.ToString(temperature)
+            cast.ToString(utilization)
+            cast.ToString(memoryUsed)
+            humanize.Bytes(memoryUsed),
+            cast.ToString(clockCore)
+            cast.ToString(clockMemory)
+            cast.ToString(PCIThroughputRX)
+            cast.ToString(PCIThroughputTX)
+            cast.ToString(numProcesses)
 						cast.ToString(memoryUsed),
-						humanize.Bytes(memoryUsed),
-						cast.ToString(memoryFree),
-						humanize.Bytes(memoryFree),
 					},
 				)
 			}
 		}
-		totalUsed = append(totalUsed, currTotalUsed)
-		totalFree = append(totalFree, currTotalFree)
-		totalEntries = append(totalEntries, uint64(len(dev.entries)))
+		totalPower = append(totalPower, currTotalPower)
+		totalTemperature = append(totalTemperature, currTotalTemperature)
+		totalUtilization = append(totalUtilization, currTotalUtilization)
+		totalMemoryUsed = append(totalMemoryUsed, currTotalMemoryUsed)
+		totalClockCore = append(totalClockCore, currTotalClockCore)
+		totalClockMemory = append(totalClockMemory, currTotalClockMemory)
+		totalPCIThroughputRX = append(totalPCIThroughputRX, currTotalPCIThroughputRX)
+		totalPCIThroughputTX = append(totalPCIThroughputTX, currTotalPCIThroughputTX)
+		totalNumProcesses = append(totalNumProcesses, currTotalNumProcesses)
+    totalEntries = append(totalEntries, uint64(len(dev.entries)))
+
 	}
-	if fullOutput {
-		rows = append(
-			rows,
-			rowDivider,
-		)
-		for ii, dev := range m.devices {
-			devIdx := cast.ToString(dev.index)
-			rows = append(
-				rows,
-				[]string{
-					devIdx,
-					"total",
-					cast.ToString(totalUsed[ii]),
-					humanize.Bytes(totalUsed[ii]),
-					cast.ToString(totalFree[ii]),
-					humanize.Bytes(totalFree[ii]),
-				},
-			)
-		}
-		rows = append(
-			rows,
-			rowDivider,
-		)
-	}
+	// if fullOutput {
+	// 	rows = append(
+	// 		rows,
+	// 		rowDivider,
+	// 	)
+	// 	for ii, dev := range m.devices {
+	// 		devIdx := cast.ToString(dev.index)
+	// 		rows = append(
+	// 			rows,
+	// 			[]string{
+	// 				devIdx,
+	// 				"peak",
+	// 			},
+	// 		)
+	// 	}
+	// 	rows = append(
+	// 		rows,
+	// 		rowDivider,
+	// 	)
+	// }
 	for ii, dev := range m.devices {
 		devIdx := cast.ToString(dev.index)
-		averageUsed := uint64(float64(totalUsed[ii]) / float64(totalEntries[ii]))
-		averageFree := uint64(float64(totalFree[ii]) / float64(totalEntries[ii]))
+		averagePower := uint64(float64(totalPower[ii]) / float64(totalEntries[ii]))
+		averageTemperature := uint64(float64(totalTemperature[ii]) / float64(totalEntries[ii]))
+		averageUtilization := uint64(float64(totalUtilization[ii]) / float64(totalEntries[ii]))
+		averageMemoryUsed := uint64(float64(totalMemoryUsed[ii]) / float64(totalEntries[ii]))
+		averageClockCore := uint64(float64(totalClockCore[ii]) / float64(totalEntries[ii]))
+		averageClockMemory := uint64(float64(totalClockMemory[ii]) / float64(totalEntries[ii]))
+		averagePCIThroughputRX := uint64(float64(totalPCIThroughputRX[ii]) / float64(totalEntries[ii]))
+		averagePCIThroughputTX := uint64(float64(totalPCIThroughputTX[ii]) / float64(totalEntries[ii]))
+		averageNumProcesses := uint64(float64(totalNumProcesses[ii]) / float64(totalEntries[ii]))
 		rows = append(
 			rows,
 			[]string{
 				devIdx,
 				"average",
-				cast.ToString(averageUsed),
-				humanize.Bytes(averageUsed),
-				cast.ToString(averageFree),
-				humanize.Bytes(averageFree),
+				cast.ToString(averagePower),
+				cast.ToString(averageTemperature),
+				cast.ToString(averageUtilization),
+        cast.ToString(averageMemoryUsed),
+        humanize.Bytes(averageMemoryUsed),
+				cast.ToString(averageClockCore),
+				cast.ToString(averageClockMemory),
+				cast.ToString(averagePCIThroughputRX),
+				cast.ToString(averagePCIThroughputTX),
+				cast.ToString(averageNumProcesses),
 			},
 		)
 	}
