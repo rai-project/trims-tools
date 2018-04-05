d43dae08a70d296d1a9ef50b50cacd8b837bb09e
Abdul Dakkak <abduld@wolfram.com>
2018-04-05 13:03:45 -0500

    pass

--- scripts/compare_large_models.sh
+++ scripts/compare_large_models.sh
@@ -134,6 +134,30 @@ export UPRD_PID=$!
 
 sleep 10
 
+./main client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_454x454_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_454x454_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_454x454_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_454x454_1.0
+
+kill -9 $UPRD_PID
+
+killall uprd
+killall main
+
+sleep 10
+
+##########################################################
+##########################################################
+
+echo "##########################################################"
+echo "##########################################################"
+
+./main server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
+
+export UPRD_PID=$!
+
+sleep 10
+
 ./main client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_227x227_1.0
 ./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_227x227_1.0
 ./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_227x227_1.0
