fda2d0323f71558e13aad44ae1aa2d0787c4cd5b
Abdul Dakkak <abduld@wolfram.com>
2018-04-05 11:45:01 -0500

    pass

--- scripts/compare_large_models.sh
+++ scripts/compare_large_models.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
 # run the server using
-# go run main.go server run -d --memory_percentage=0.9 --estimate_with_internal_memory=false
+# ./main server run -d --memory_percentage=0.9 --estimate_with_internal_memory=false
+
+killall bin/uprd
+
+
+go build main.go
 
 echo "##########################################################"
 echo "##########################################################"
 
-go run main.go server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
+./main server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
 
 export UPRD_PID=$!
 
-sleep 10s
+sleep 600
 
-go run main.go client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_896x896_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_896x896_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_896x896_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_896x896_1.0
+./main client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_896x896_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_896x896_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_896x896_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_896x896_1.0
 
 kill -9 $UPRD_PID
 
@@ -25,16 +30,16 @@ kill -9 $UPRD_PID
 echo "##########################################################"
 echo "##########################################################"
 
-go run main.go server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
+./main server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
 
 export UPRD_PID=$!
 
-sleep 10s
+sleep 600
 
-go run main.go client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_681x681_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_681x681_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_681x681_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_681x681_1.0
+./main client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_681x681_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_681x681_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_681x681_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_681x681_1.0
 
 kill -9 $UPRD_PID
 
@@ -44,16 +49,16 @@ kill -9 $UPRD_PID
 echo "##########################################################"
 echo "##########################################################"
 
-go run main.go server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
+./main server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
 
 export UPRD_PID=$!
 
-sleep 10s
+sleep 600
 
-go run main.go client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_672x672_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_672x672_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_672x672_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_672x672_1.0
+./main client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_672x672_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_672x672_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_672x672_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_672x672_1.0
 
 kill -9 $UPRD_PID
 
@@ -63,16 +68,16 @@ kill -9 $UPRD_PID
 echo "##########################################################"
 echo "##########################################################"
 
-go run main.go server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
+./main server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
 
 export UPRD_PID=$!
 
-sleep 10s
+sleep 600
 
-go run main.go client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_908x908_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_908x908_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_908x908_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_908x908_1.0
+./main client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_908x908_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_908x908_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_908x908_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_908x908_1.0
 
 
 kill -9 $UPRD_PID
@@ -83,16 +88,16 @@ kill -9 $UPRD_PID
 echo "##########################################################"
 echo "##########################################################"
 
-go run main.go server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
+./main server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
 
 export UPRD_PID=$!
 
-sleep 10s
+sleep 600
 
-go run main.go client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_448x448_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_448x448_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_448x448_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_448x448_1.0
+./main client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_448x448_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_448x448_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_448x448_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_448x448_1.0
 
 kill -9 $UPRD_PID
 
@@ -102,16 +107,16 @@ kill -9 $UPRD_PID
 echo "##########################################################"
 echo "##########################################################"
 
-go run main.go server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
+./main server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
 
 export UPRD_PID=$!
 
-sleep 10s
+sleep 600
 
-go run main.go client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_227x227_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_227x227_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_227x227_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_227x227_1.0
+./main client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_227x227_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_227x227_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_227x227_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_alexnet_227x227_1.0
 
 kill -9 $UPRD_PID
 
@@ -121,16 +126,16 @@ kill -9 $UPRD_PID
 echo "##########################################################"
 echo "##########################################################"
 
-go run main.go server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
+./main server run --memory_percentage=0.9 --estimate_with_internal_memory=false &
 
 export UPRD_PID=$!
 
-sleep 10s
+sleep 600
 
-go run main.go client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_224x224_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_224x224_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_224x224_1.0
-go run main.go client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_224x224_1.0
+./main client compare --run_original=true  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_first_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_224x224_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_second_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_224x224_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_third_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_224x224_1.0
+./main client compare --run_original=false  --monitor_memory=false --concurrent=1  --profile_output=large_model_compare_forth_iteration --experiment_description="large model comparison" --large_models=true --models=large_vgg16_224x224_1.0
 
 kill -9 $UPRD_PID
 
